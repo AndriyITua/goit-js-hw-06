735249c6b28406bff59fdb62c943149d15674eb5
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2024-01-22 20:34:15 +0200

    final

---
+++ js/task-2.js
@@ -0,0 +1,28 @@
+class Storage {
+    #items = [];
+    
+    constructor([productA, productB, productC]) {
+        this.#items = [productA, productB, productC]; 
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        if (this.#items.includes(itemToRemove)){
+            this.#items = this.#items.filter(i => i !== itemToRemove);
+        }
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
